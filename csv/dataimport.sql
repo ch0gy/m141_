135a2fe9972b6a0a9aa633e1faa41af6e536c4f1
ch0gy <trinle.chophel@edu.tbz.ch>
2025-07-04 11:33:07 +0200

    new file:   README.md 	new file:   csv/backpacker_db_update.sql 	new file:   csv/berechtgungen 	new file:   csv/bp_db_fol/.DS_Store 	new file:   csv/bp_db_fol/tbl_benutzer.csv 	new file:   csv/bp_db_fol/tbl_buchung.csv 	new file:   csv/bp_db_fol/tbl_land.csv 	new file:   csv/bp_db_fol/tbl_leistung.csv 	new file:   csv/bp_db_fol/tbl_personen.csv 	new file:   csv/bp_db_fol/tbl_positionen.csv 	new file:   csv/dataimport.sql

---
+++ csv/dataimport.sql
@@ -0,0 +1,62 @@
+LOAD DATA LOCAL INFILE 'C:\\Users\\tchophel\\Desktop\\sache\\TBZ\\md\\141\\lb3\\csv\\bp_db_fol\\tbl_personen.csv'
+INTO TABLE tbl_personen
+CHARACTER SET utf8mb4
+FIELDS TERMINATED BY ';'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+LOAD DATA LOCAL INFILE 'C:\\Users\\tchophel\\Desktop\\sache\\TBZ\\md\\141\\lb3\\csv\\bp_db_fol\\tbl_land.csv'
+INTO TABLE tbl_land
+CHARACTER SET utf8mb4
+FIELDS TERMINATED BY ';'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'C:\\Users\\tchophel\\Desktop\\sache\\TBZ\\md\\141\\lb3\\csv\\bp_db_fol\\tbl_buchung.csv'
+INTO TABLE tbl_buchung
+CHARACTER SET utf8mb4
+FIELDS TERMINATED BY ';'
+ENCLOSED BY ''
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(Buchungs_ID, Personen_FS, Land_FS, Ankunft, Abreise);
+
+
+LOAD DATA LOCAL INFILE 'C:\\Users\\tchophel\\Desktop\\sache\\TBZ\\md\\141\\lb3\\csv\\bp_db_fol\\tbl_leistung.csv' 
+INTO TABLE tbl_leistung
+CHARACTER SET utf8mb4
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(LeistungID, Beschreibung);
+
+
+LOAD DATA LOCAL INFILE 'C:\\Users\\tchophel\\Desktop\\sache\\TBZ\\md\\141\\lb3\\csv\\bp_db_fol\\tbl_benutzer.csv'
+INTO TABLE tbl_benutzer
+CHARACTER SET utf8mb4
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(Benutzer_ID, Benutzername, PasswortHash, Vorname, Name, Benutzergruppe, erfasst, @deaktiviert, aktiv)
+SET
+  deaktiviert = NULLIF(@deaktiviert, '');
+
+
+
+LOAD DATA LOCAL INFILE 'C:\\Users\\tchophel\\Desktop\\sache\\TBZ\\md\\141\\lb3\\csv\\bp_db_fol\\tbl_positionen.csv'
+INTO TABLE tbl_positionen
+CHARACTER SET utf8mb4
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(Positions_ID, Buchungs_FS, Konto, Anzahl, Preis, Rabatt, Benutzer_FS, erfasst, Leistung_Text, @Leistung_FS)
+SET
+  Leistung_FS = NULLIF(@Leistung_FS, '');
+
+
+UPDATE tbl_benutzer
+SET PasswortHash = SHA2(PasswortHash, 256);
